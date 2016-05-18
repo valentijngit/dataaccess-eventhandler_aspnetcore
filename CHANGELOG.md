3e79b93b81aeaf73cfcb72ff75791eff42ca049f
Koen Stroobants <koen.stroobants@digipolis.be>
2016-05-18 14:36:16 +0200

    Generated

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# DataAccess.EventHandler Toolbox
+
+## 1.0.0
+
+- initial version
---
+++ src/Digipolis.Toolbox.DataAccess.EventHandler/Properties/AssemblyInfo.cs
@@ -0,0 +1,1 @@
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Digipolis.Toolbox.DataAccess.EventHandler.UnitTests")]
